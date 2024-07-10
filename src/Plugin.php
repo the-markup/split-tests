9dd3cf96eee0df00a21895486970c6ba063bbc67
Dan Phiffer <dan@phiffer.org>
2024-07-10 12:37:07 -0400

    db table with test/convert entries

--- src/Plugin.php
+++ src/Plugin.php
@@ -29,12 +29,55 @@ class Plugin {
      */
     protected $chosen_variants = [];
 
+    /**
+     * Ensure the conversion is only counted once.
+     *
+     * @var bool
+     */
+    protected $converted = false;
+
+    /**
+     * Avoid double-applying the variant post filter.
+     *
+     * @var bool
+     */
+    protected $ignore_post_variants = false;
+
+    /**
+     * Database version lets us apply updates to the db schema in the future.
+     *
+     * @var int
+     */
+    protected $db_version = 1;
+
 	/**
 	 * Setup filters.
 	 *
 	 * @return void
 	 */
 	function __construct() {
+        $this->setup_db();
+        $this->setup_hooks();
+    }
+
+    /**
+     * Checks if the database schema requires updating, then updates it if necessary.
+     *
+     * @return void
+     */
+    function setup_db() {
+        $curr_db_version = get_option('split_tests_db_version', 0);
+        if ($curr_db_version < $this->db_version) {
+            $this->migrate_db($curr_db_version);
+        }
+    }
+
+    /**
+     * Setup action/filter handlers.
+     *
+     * @return void
+     */
+    function setup_hooks() {
         // This filter is ours, for adjusting individual posts with a variant
         add_filter('split_tests_post_variant', [$this, 'post_variant']);
 
@@ -49,6 +92,9 @@ class Plugin {
         
         // Permalink filter
         add_filter('pre_post_link', [$this, 'pre_post_link'], 10, 2);
+
+        // Add split_test posts whenever you save a post with a split test
+        add_action('save_post', [$this, 'save_post'], 10, 2);
         
         // ACF JSON path
         add_filter('acf/settings/load_json', [$this, 'load_acf_json']);
@@ -95,6 +141,12 @@ class Plugin {
             return $posts;
         }
 
+        // Avoid double-applying the variant filter.
+        // (See also: 'check_variant_url_slugs' called below.)
+        if ($this->ignore_post_variants) {
+            return $posts;
+        }
+
         // Found a non-existant slug? Let's check for variant 'url_slug'
         // postmeta values.
         if (empty($posts) && !empty($this->request_slug)) {
@@ -132,31 +184,52 @@ class Plugin {
      * @return array
      */
     function get_variant($post) {
+        if (is_single() && $post->post_type == 'post') {
+            $variant = null;
+            if (! empty($this->chosen_variants[$post->ID])) {
+                $variant = $this->chosen_variants[$post->ID];
+                $variant_index = $variant['index'];
+            } else {
+                $variant_index = 0;
+            }
+            if (! $this->converted) {
+                $this->converted = true;
+                $variants = get_field('title_variants', $post->ID);
+                if (count($variants) > 0) {
+                    $this->variant_convert($post->ID, 'title', $variant_index);
+                }
+            }
+            return $variant;
+        }
+
         // Check if we already chose a variant for this post.
         if (! empty($this->chosen_variants[$post->ID])) {
             return $this->chosen_variants[$post->ID];
         }
 
-        if (is_single()) {
-            return null;
-        }
-
         // Query for any variants on this post.
-        $variants = get_field('title_variants', $post->ID);
-        if (empty($variants)) {
+        $_variants = get_field('title_variants', $post->ID);
+        if (empty($_variants)) {
             return null;
         }
 
         // Add a choice from the post's original values.
-        $variants[] = [
-            'headline' => $post->post_title,
-            'url_slug' => $post->post_name
+        $variants = [
+            [
+                'headline' => $post->post_title,
+                'url_slug' => $post->post_name
+            ],
+            ... $_variants
         ];
         $index = rand(0, count($variants) - 1);
         $variant = $variants[$index];
 
-        // Cache this variant choice
+        // Cache this variant choice.
         $this->chosen_variants[$post->ID] = $variant;
+
+        // Boop the 'test' variable for this variant.
+        $this->variant_test($post->ID, 'title', $index);
+
         return $variant;
     }
 
@@ -191,20 +264,130 @@ class Plugin {
 		if (empty($post_ids)) {
 			return [];
 		}
+
         $query_vars = $query->query_vars;
         unset($query_vars['name']);
         $query_vars['post__in'] = $post_ids;
+        $this->ignore_post_variants = true;
         $posts = get_posts($query_vars);
+        $this->ignore_post_variants = false;
+
         if (is_single() && count($posts) == 1) {
             $variants = get_field('title_variants', $posts[0]->ID);
-            foreach ($variants as $variant) {
+            foreach ($variants as $index => $variant) {
                 if ($variant['url_slug'] == $request_slug) {
+                    // Add one to the index to account for index 0 being 'unmodified.'
+                    $variant['index'] = $index + 1;
                     $this->chosen_variants[$posts[0]->ID] = $variant;
                     break;
                 }
             }
         }
         return $posts;
+    }
+
+    /**
+     * Handle 'save_post' action whenever someone saves a post.
+     *
+     * @return void
+     */
+    function save_post($post_id, $post) {
+        if ($post->post_type !== 'post') {
+            return;
+        }
+
+        $variants = get_field('title_variants', $post_id);
+        if (empty($variants)) {
+            return;
+        }
+
+        $split_test_post_id = get_post_meta($post_id, 'split_test_post_id', true);
+        if (empty($split_test_post_id)) {
+            $split_test_post_id = wp_insert_post([
+                'post_type' => 'split_test',
+                'post_title' => "Post $post_id Title",
+            ]);
+            update_post_meta($post_id, 'split_test_post_id', $split_test_post_id);
+            update_post_meta($split_test_post_id, 'target_post_id', $post_id);
+            update_post_meta($split_test_post_id, 'variant_count', count($variants) + 1);
+            for ($i = 0; $i <= count($variants); $i++) {
+                update_post_meta($split_test_post_id, "variant_{$i}_test", 0);
+                update_post_meta($split_test_post_id, "variant_{$i}_convert", 0);
+            }
+        } else {
+            $old_count = get_post_meta($split_test_post_id, 'variant_count', true);
+            if ($old_count < $new_count) {
+                for ($i = $old_count; $i < $new_count; $i++) {
+                    update_post_meta($split_test_post_id, "variant_{$i}_test", 0);
+                    update_post_meta($split_test_post_id, "variant_{$i}_convert", 0);
+                }
+            }
+        }
+    }
+
+    /**
+     * Update, or "boop," a variant statistic ('test' or 'convert') for a given
+     * split test post ID.
+     *
+     * @return void
+     */
+    function boop_split_test($split_test_id, $test_type, $variant_index, $boop_type) {
+        global $wpdb;
+        $now = wp_date('Y-m-d H:i:s');
+        $wpdb->query($wpdb->prepare("
+            INSERT INTO {$wpdb->prefix}split_tests
+            (split_test_id, test_type, variant_index, test_or_convert, created_time)
+            VALUES (%d, %s, %d, %s, %s)
+        ", $split_test_id, $test_type, $variant_index, $boop_type, $now));
+    }
+
+    /**
+     * "Boop" the 'test' variable for a given target post ID.
+     *
+     * @return void
+     */
+    function variant_test($target_id, $test_type, $variant_index) {
+        $split_test_id = get_post_meta($target_id, 'split_test_post_id', true);
+        if (!$split_test_id) {
+            return;
+        }
+        $this->boop_split_test($split_test_id, $test_type, $variant_index, 'test');
+    }
+
+    /**
+     * "Boop" the 'convert' variable for a given target post ID.
+     *
+     * @return void
+     */
+    function variant_convert($target_id, $test_type, $variant_index) {
+        $split_test_id = get_post_meta($target_id, 'split_test_post_id', true);
+        if (!$split_test_id) {
+            return;
+        }
+        $this->boop_split_test($split_test_id, $test_type, $variant_index, 'convert');
+    }
+
+    /**
+     * Migrates the database schema based on a version number.
+     *
+     * @return void
+     */
+    function migrate_db($curr_version) {
+        global $wpdb;
+        if ($curr_version < 1) {
+            $table_name = $wpdb->prefix . 'split_tests';
+            $charset_collate = $wpdb->get_charset_collate();
+            $sql = "CREATE TABLE $table_name (
+                split_test_id BIGINT(20) UNSIGNED NOT NULL,
+                test_type VARCHAR(255) NOT NULL,
+                variant_index TINYINT UNSIGNED NOT NULL,
+                test_or_convert ENUM('test', 'convert'),
+                created_time DATETIME DEFAULT '0000-00-00 00:00:00' NOT NULL
+            ) $charset_collate;";
+        }
+	    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	    dbDelta($sql);
+        update_option('split_tests_db_version', $this->db_version);
     }
 
     /**
@@ -232,7 +415,7 @@ class Plugin {
             'view_items' => 'View %name%',
             'search_items' => 'Search %name%',
             'not_found' => 'No %name% found',
-            'not_found_in_trash' => 'No %name found in trash',
+            'not_found_in_trash' => 'No %name% found in trash',
             'parent_item_colon' => 'Parent %singular_name%:',
             'all_items' => 'All %name%',
             'archives' => '%singular_name% Archives',
